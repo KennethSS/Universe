ebca41dea9a5310a62961815a7ef926c3ed2ef02
Kenneth <wlsrhkd4023@gmail.com>
2021-07-06 21:08:25 +0900

    add glide

---
+++ universe/src/main/java/com/solar/universe/extension/ImageViewExt.kt
@@ -0,0 +1,30 @@
+package com.solar.universe.extension
+
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.solar.universe.glide.GlideApp
+import com.solar.universe.glide.GlideAppModule
+
+@BindingAdapter(value = ["load"])
+fun ImageView.loadUrl(url: String?) {
+    loadImage(url)
+}
+
+@BindingAdapter(value = ["loadCircle"])
+fun ImageView.loadUrlCircle(url: String?) {
+    loadImage(url)
+}
+
+private fun ImageView.loadImage(
+    imageUrl: String?,
+    @DrawableRes placeHolder: Int = 0,
+    centerCrop: Boolean = false
+) = Glide.with(this)
+    .load(imageUrl)
+    .thumbnail(0.1f)
+    .placeholder(placeHolder)
+    .also { if (centerCrop) it.apply(RequestOptions.circleCropTransform()) }
+    .into(this)
