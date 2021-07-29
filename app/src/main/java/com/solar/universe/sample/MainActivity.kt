3e5c969dc13cd5ae3a37afb5f69cd845d96a3c71
KennethSS <39362939+KennethSS@users.noreply.github.com>
2021-07-29 10:57:33 +0900

    add fragment 실패 케이스

--- app/src/main/java/com/solar/universe/sample/MainActivity.kt
+++ app/src/main/java/com/solar/universe/sample/MainActivity.kt
@@ -3,11 +3,16 @@ package com.solar.universe.sample
 import android.os.Bundle
 import com.solar.universe.binding.activity.UniverseViewActivity
 import com.solar.universe.sample.databinding.ActivityMainBinding
+import com.solar.universe.sample.recyclerview.TestFragment
 
 class MainActivity : UniverseViewActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.tvSample.text = "HI"
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container_view, TestFragment(9))
+            .commitNow()
     }
 }
---
+++ app/src/main/java/com/solar/universe/sample/recyclerview/TestFragment.kt
@@ -0,0 +1,8 @@
+package com.solar.universe.sample.recyclerview
+
+import androidx.fragment.app.Fragment
+
+
+class TestFragment(val number: Int) : Fragment() {
+
+}
