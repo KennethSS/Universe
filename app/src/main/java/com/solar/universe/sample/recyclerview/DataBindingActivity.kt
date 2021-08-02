2ea660556ea96e2fb996c3dab799025ce0c46d54
KennethSS <39362939+KennethSS@users.noreply.github.com>
2021-08-02 14:58:16 +0900

    add databinding sample

---
+++ app/src/main/java/com/solar/universe/sample/recyclerview/DataBindingActivity.kt
@@ -0,0 +1,15 @@
+package com.solar.universe.sample.recyclerview
+
+import android.os.Bundle
+import com.solar.universe.binding.activity.UniverseDataActivity
+import com.solar.universe.sample.R
+import com.solar.universe.sample.databinding.ActivityDataBindingBinding
+
+class DataBindingActivity :
+  UniverseDataActivity<ActivityDataBindingBinding>(R.layout.activity_data_binding) {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+  }
+}
